6ff39067cebf747af98c1fd450eb29cdd250d8d5
Javier Celada <celada.jcm.javier@gmail.com>
2024-09-19 10:16:01 +0200

    Validation and saving user

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,7 @@
+const validate = require("../validation/validate");
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+
 const test = (req, res) => {
     return res.status(200).send({
         message: "Send from user controller"
@@ -8,7 +12,6 @@ const test = (req, res) => {
 const signup = (req, res) => {
     // Get data
     let params = req.body;
-    console.log(params);
     // Required data
     if (!params.name || !params.nick || !params.email || !params.password) {
         return res.status(400).send({
@@ -17,13 +20,56 @@ const signup = (req, res) => {
         })
     }
     // Validate data
-    
+    try {
+        validate(params);
+    } catch (error) {
+        console.log(error);
+        return res.status(400).send({
+            status: "error",
+            message: "Error validating fields"
+        })
+    }
     // User duplicate
-    // Password encryption
-    // Create user model
-    // Save user
-    // Clean response
-    return res.status(200).send({ status: "success", message: "User registered" });
+    User.find({
+        $or: [
+            { email: params.email.toLowerCase() },
+            { email: params.nick.toLowerCase() }
+        ]
+    }).then(async users => {
+        if (users && users.length > 1) {
+            return res.status(400).send({
+                status: "error",
+                message: "Duplicated user"
+            })
+        }
+        // Password encryption
+        let pwd = await bcrypt.hash(params.password, 10);
+        params.password = pwd;
+        // Create user model
+        let userToSave = new User(params);
+        // Save user
+        userToSave.save().then(userStored => {
+            if (!userStored) {
+                return res.status(500).send({
+                    status: "error",
+                    message: "Error saving user"
+                })
+            }
+            return res.status(200).send({ status: "success", message: "User registered", user: userStored.email });
+        }).catch(error => {
+            console.log(error);
+            return res.status(500).send({
+                status: "error",
+                message: "Error saving user"
+            })
+        });
+    }).catch(error => {
+        console.log(error);
+        return res.status(500).send({
+            status: "error",
+            message: "Error checking for duplicated user"
+        })
+    });
 };
 
 module.exports = {
--- validation/validate.js
+++ validation/validate.js
@@ -4,6 +4,19 @@ const validate = (params) => {
         && validator.isLength(params.name, { min: 3, max: 20 })
         && validator.isAlpha(params.name, "es-ES");
     let nick = !validator.isEmpty(params.nick)
-        && validator.isLength(params.nick, { min: 3, max: 20 })
+        && validator.isLength(params.nick, { min: 3, max: 60 })
         && validator.isAlpha(params.nick, "es-ES");
+    let email = !validator.isEmpty(params.email)
+        && validator.isEmail(params.email);
+    let password = !validator.isEmpty(params.password);
+    if (params.surname) {
+        let surname = !validator.isEmpty(params.surname)
+            && validator.isLength(params.surname, { min: 3, max: 20 })
+            && validator.isAlpha(params.surname, "es-ES");
+        if (!surname) throw new Error("Error validating surname");
+    }
+
+    if (!name || !nick || !email || !password) throw new Error("Error validating required fields");
 }
+
+module.exports = validate;
